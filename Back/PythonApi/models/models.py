1634cb8016d2374081a2eb2b062e1646be176d6b
OscarDOE <2905334300101@ingenieria.usac.edu.gt>
2024-04-03 23:40:43 -0600

    Update models.py

--- Back/PythonApi/models/models.py
+++ Back/PythonApi/models/models.py
@@ -1,29 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class Login(BaseModel):
     user: str
     password: str
 
-class Profile(BaseModel):
-    user: str
-    name: str
-    photo: str
-
 class id(BaseModel):
     user: str
 
-class Createalbum(BaseModel):
-    id: int
-    album: str
-
-class Editalbum(BaseModel):
-    id: int
-    id_album: int
-    newalbum: str
-
-
-
 class Chat(BaseModel):
     message:str
+
+class Translate(BaseModel):
+    message: str
